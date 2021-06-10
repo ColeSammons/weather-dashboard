c1c798b5ef35f0714ddf006a98ef48a74c8721d1
Cole Sammons <colemsammons@gmail.com>
2021-06-10 14:41:47 -0700

    search bar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,29 @@
 var city = "Mesa";
-var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff2a715a55507c8128294ebc5723fc0a";
 var cities = [];
 
-fetch(urlWeather).then(function (response) {
-    console.log(response);
-    return response.json();
-}).then(function (data) {
-    console.log(data.dt);
-    var currentDate = data.dt
-    displayDate(currentDate);
-})
+var getData = function (city) {
+    var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff2a715a55507c8128294ebc5723fc0a";
 
-var displayDate = function(unix) {
+    fetch(urlWeather).then(function (response) {
+        console.log(response);
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
+        console.log(data.name);//city
+        console.log(data.main.temp);//temp
+        console.log(data.wind.speed);//wind speed
+        console.log(data.main.humidity);
+        console.log();
+        console.log();
+        console.log();
+        displayDate(data.dt);//date
+    })
+}
+
+var displayDate = function (unix) {
     var date = new Date(unix * 1000);
     var timeStamp = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
     console.log(timeStamp);
 }
+
+getData(city);
