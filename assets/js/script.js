f0e387662af03f9d16844a42d72f322e80366028
Cole Sammons <colemsammons@gmail.com>
2021-06-10 16:03:43 -0700

    fetch call for UV

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,30 @@
-var city = "Mesa";
 var cities = [];
 
 var getData = function (city) {
-    var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff2a715a55507c8128294ebc5723fc0a";
+    var urlCurrentWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=ff2a715a55507c8128294ebc5723fc0a";
 
-    fetch(urlWeather).then(function (response) {
-        console.log(response);
-        return response.json();
-    }).then(function (data) {
-        console.log(data);
-        console.log(data.name);//city
-        console.log(data.main.temp);//temp
-        console.log(data.wind.speed);//wind speed
-        console.log(data.main.humidity);
-        console.log();
-        console.log();
-        console.log();
-        displayDate(data.dt);//date
-    })
+    fetch(urlCurrentWeather).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                console.log(data);
+                console.log(data.name);//city
+                console.log(data.main.temp);//temp
+                console.log(data.wind.speed);//wind speed
+                console.log(data.main.humidity);//humidity
+                console.log(data.coord.lat);//latitude
+                console.log(data.coord.lon);//longitude
+                displayDate(data.dt);//date
+                getUV(data.coord.lat, data.coord.lon)
+                // var imgIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png")
+                // $("#currentCity").append(imgIcon);
+            })
+        }
+        else {
+            alert("City was not found");
+        }
+    }).catch(function (error) {
+        alert("Unable to connect to weather api");
+    });
 }
 
 var displayDate = function (unix) {
@@ -26,4 +33,28 @@ var displayDate = function (unix) {
     console.log(timeStamp);
 }
 
-getData(city);
+var getUV = function(lat, lon) {
+    varCityUV = "https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=ff2a715a55507c8128294ebc5723fc0a";
+
+    fetch(varCityUV).then(function(response) {
+        if(response.ok) {
+            response.json().then(function (data) {
+                console.log(data.current.uvi)
+            })
+        }
+        else {
+            alert("Incorrect latitude/longitude");
+        }
+    }).catch(function (error) {
+        alert("Unable to connect to weather api");
+    });
+}
+
+
+$(".formInput").on("submit", function () {
+    event.preventDefault();
+    console.log($("#inputCity").val());
+    let city = $("#inputCity").val();
+    getData(city);
+})
+
